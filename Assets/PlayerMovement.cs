bf162b2a355301ec3b76d7672ea77a2f04dfab84
Bruno Decnop <brunodecnop@id.uff.br>
2022-09-28 12:15:56 -0300

    Correção Commit Desenvolvimento Mecanica de Jogador e Dado

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public GameObject jogador;
+    protected int actualPosition = 0;
+
+
+    public void StartMoveJogador(int dado) {
+        StartCoroutine(MoveJogador(dado));
+    }
+
+    public IEnumerator MoveJogador(int dado) {
+        for(int i = 1; i <= dado; i++) {
+            if(actualPosition == 39) {
+                actualPosition = 0;
+                jogador.transform.position = tiles[0].transform.position;
+            } else { 
+            jogador.transform.position = tiles[actualPosition +1].transform.position;
+            actualPosition++;
+              }
+
+            yield return new WaitForSeconds(0.5f);
+
+        }
+    }
+  
+}
